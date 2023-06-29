34ffb05c081bef436a447a18042734caa2fe372b
pushpitha <pushpithagowda776@gmail.com>
2023-06-29 12:33:29 +0530

    Find the largest number

---
+++ LargestNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class LargestNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a, b & c: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int max = a;
+    if (b > max) {
+      max = b;
+    }
+    if (c > max) {
+      max = c;
+    }
+    System.out.println(max + " is largest");
+    sc.close();
+  }
+}
