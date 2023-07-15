676732fc329b25337de3f8edea42ee1896460da9
pushpitha <pushpithagowda776@gmail.com>
2023-07-15 09:29:52 +0530

    Binary Search in Sorted Array

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class BinarySearch {
+  public static void main(String[] args) {
+    int arr[] = {1,2,3,4,5,6,7,8,9,10,32};
+    System.out.println("Enter the number to be searched: ");
+    Scanner sc = new Scanner(System.in);
+    int target = sc.nextInt();
+    int ans = binarySearch(arr, target);
+    if (ans == -1) {
+      System.out.println("Element not found");
+    } else {
+      System.out.println("Element found at: " + ans + " position.");
+    }
+  }
+
+  static int binarySearch(int arr[], int target) {
+    int start = 0;
+    int end = arr.length-1;
+
+    while(start <= end) {
+      int mid = start + (end - start) / 2;
+      if(target < arr[mid]) {
+        end = mid - 1;
+      } else if(target > arr[mid]) {
+        start = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+}
