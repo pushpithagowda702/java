595019213e2a1f53e96bd0694e5e1a1565a60a28
pushpitha <pushpithagowda776@gmail.com>
2023-11-25 14:40:01 +0530

    Object Cloning

--- Cloning.java
+++ Cloning.java
@@ -1,15 +1,28 @@
 class Human implements Cloneable {
   String name;
   int age;
+  int arr[];
 
   Human(String name, int age) {
     this.name = name;
     this.age = age;
+    this.arr = new int[] {1,2,3,4,5};
   }
 
+  //Shallow copy
   public Human clone() throws CloneNotSupportedException {
     return (Human) super.clone();
   }
+
+  // Deep copy 
+  // public Human clone() throws CloneNotSupportedException {
+  //  Human twin = (Human) super.clone();
+  // twin.arr = new int[twin.arr.length]; 
+  // for(int i=0; i<twin.arr.length; i++) {
+  //   twin.arr[i] = this.arr[i];
+  // }
+  //   return twin;
+  // }
 }
 
 class Cloning {
