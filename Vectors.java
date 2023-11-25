beb00e8cd3921d40885bdaccc3a3f45f9fc0f484
pushpitha <pushpithagowda776@gmail.com>
2023-11-25 15:38:02 +0530

    Vectors

---
+++ Vectors.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Vector;
+
+class Vectors {
+  public static void main(String[] args) {
+    List<Integer> l = new Vector<>();
+    l.add(45);
+    l.add(36);
+    l.add(43);
+    l.add(32);
+
+    System.out.println(l);
+  }
+}
