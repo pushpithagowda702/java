efaeee7883f1351aef1c6952e1134f2d007d3ae9
pushpitha <pushpithagowda776@gmail.com>
2023-11-27 17:20:22 +0530

    Fibonacci numbers using dynamic programming

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -8,12 +8,16 @@ public class Fibonacci {
       array[i] = -1;
     }
     System.out.println(fib(n, array));
+    System.out.println(fibBU(n, array));
+    int a = 0;
+    int b = 1;
+    System.out.println(fib(n, a, b));
   }
 
   // Top-down approach
-  
+
   static int fib(int n, int[] array) {
-    if(n <= 1) {
+    if(n < 2) {
       array[n] = n;
       return array[n];
     }
@@ -24,6 +28,29 @@ public class Fibonacci {
 
     array[n] = fib(n-1, array) + fib(n-2, array);
     return array[n];
+  }
+
+  // Bottom-Up approach
+
+  static int fibBU(int n, int[] array) {
+    
+    for(int i=2; i<n; i++) {
+      array[i] = array[i-1] + array[i-2];
+    }
+
+    return array[n];
+  }
+
+  // Optimum solution 
+
+  static int fib(int num, int a, int b) {
+    
+    for(int i=2; i<=num; i++) {
+      int c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
   }
   
 }
