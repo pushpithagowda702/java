350579f80c8e3e94c95666b047296dbbb7a29d10
pushpitha <pushpithagowda776@gmail.com>
2023-11-29 21:21:05 +0530

    Skip a character in a string

--- recursion/SkipChar.java
+++ recursion/SkipChar.java
@@ -6,8 +6,10 @@ public class SkipChar {
     StringBuilder sol2 = new StringBuilder();
     skipCharacter(0, "java", sol2);
     System.out.println(sol2);
+    System.out.println(skipCharacter(0, "amateur"));
+    System.out.println(skipCharacter(0, "java"));
   }
-
+  // Method 1
   static void skipCharacter(int i, String s, StringBuilder ans) {
     if (i == s.length()) {
       return;
@@ -18,6 +20,20 @@ public class SkipChar {
     } else {
       ans.append(s.charAt(i));
       skipCharacter(i+1, s, ans);
+    }
+  }
+
+  // Method 2
+
+  static String skipCharacter(int i, String s) {
+    if(i == s.length()) {
+      return "";
+    }
+
+    if(s.charAt(i) == 'a') {
+      return skipCharacter(i+1, s);
+    } else {
+      return s.charAt(i) + skipCharacter(i+1, s);
     }
   }
 }
