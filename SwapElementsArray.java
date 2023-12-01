83c9aa38b0c4e0c87fe8bd7663630098077818d6
pushpitha <pushpithagowda776@gmail.com>
2023-12-01 23:05:11 +0530

    Swap elements in an array

--- SwapElementsArray.java
+++ SwapElementsArray.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class SwapElementsArray {
   public static void main(String[] args) {
+
     int[] a = {1, 2, 5, 3, 8, 4, 73, 65};
     swap(a, 4, 5);
     System.out.println(Arrays.toString(a));
