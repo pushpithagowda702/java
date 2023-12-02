05aedaff8cfd44517109582f3bb055775b650d19
pushpitha <pushpithagowda776@gmail.com>
2023-12-02 18:18:08 +0530

    Transpose of a matrix

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -15,11 +15,11 @@ public class TransposeMatrix {
   static void transpose(int[][] matrix) {
     for(int i=0; i<matrix.length; i++) {
       for(int j=0; j<i; j++) {
-        if(i>j) {
+        
           int temp = matrix[i][j];
           matrix[i][j] = matrix[j][i];
           matrix[j][i] = temp;
-        }
+        
       }
     }
   }
