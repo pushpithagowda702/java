a97fea7c78aa605cbd6323f13f0bec171a8756f5
pushpitha <pushpithagowda776@gmail.com>
2023-12-04 23:59:41 +0530

    Reverse vowels in a string

---
+++ ReverseVowels.java
@@ -0,0 +1,21 @@
+public class ReverseVowels {
+  public static void main(String[] args) {
+    
+  }
+
+  static String reverse(String s) {
+    String str = "";
+    int i = 0;
+    int j = s.length()-1;
+    while(i<j) {
+
+      char chS = s.charAt(i);
+      char chE = s.charAt(j);
+      if(chS == 'A' || chS == 'E' || chS == 'I' || chS == 'O' || chS == 'U' || chS == 'a' || chS == 'e' || chS == 'i' || chS == 'o' || chS == 'u') {
+        
+      }
+    }
+
+    return str;
+  }
+}
