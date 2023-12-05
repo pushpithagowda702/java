7c91a14477cd1021708046c30f2d20ec77a478a7
pushpitha <pushpithagowda776@gmail.com>
2023-12-05 12:07:34 +0530

    cd /home/pushpitha/Documents/java ; /usr/bin/env /usr/lib/jvm/java-18-openjdk-amd64/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/pushpitha/.config/Code/User/workspaceStorage/fa024dd2a3c938d63b055704a1511949/redhat.java/jdt_ws/java_44253a4f/bin SkipString

--- recursion/SkipString.java
+++ recursion/SkipString.java
@@ -3,6 +3,7 @@ public class SkipString {
     String s = "applebananagreenapplepineappleguavacustardapple";
     System.out.println(skip(s));
     System.out.println();
+    System.out.println(skipString(s));
   }
 
   static String skip(String s) {
@@ -10,10 +11,22 @@ public class SkipString {
       return "";
     }
 
-    if(s.length()>=5 && s.substring(0,5).equals("apple")) {
+    if(s.length() >= 5 && s.substring(0,5).equals("apple")) {
       return skip(s.substring(1));
     } else {
       return s.substring(0,1) + " " + skip(s.substring(1));
+    }
+  }
+
+  static String skipString(String s) {
+    if(s.isEmpty()) {
+      return "";
+    }
+
+    if(s.length() >= 5 && s.substring(0,5).equals("apple")) {
+      return skipString(s.substring(5));
+    } else {
+      return s.substring(0,1) + " " + skipString(s.substring(1));
     }
   }
 }
