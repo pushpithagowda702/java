dbdc200e2c53bbd56218d738d7d3346f0d80be12
pushpitha <pushpithagowda776@gmail.com>
2023-12-12 23:51:35 +0530

    Power of Three

---
+++ LeetCode/LeetCode326.java
@@ -0,0 +1,20 @@
+public class LeetCode326 {
+  class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n == 1) {
+            return true;
+        }
+        
+        if(n <= 0 || n%3 != 0) {
+            return false;
+        }
+        
+
+        
+        while(n%3 == 0) {
+            n = n/3;
+        }
+        return n==1;
+    }
+}
+}
