1aaec23953562a6f27f512d23584d29eea2bff16
pushpitha <pushpithagowda776@gmail.com>
2024-01-13 17:16:07 +0530

    Cycle in a linked list

---
+++ LeetCode/LeetCode142.java
@@ -0,0 +1,33 @@
+public class LeetCode142 {
+  public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode fast = head, slow = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) {
+                break;
+            }
+        }
+        
+        if(fast == null || fast.next == null) {
+            return null;
+        }
+        
+        slow = head;
+        
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+}
+}
